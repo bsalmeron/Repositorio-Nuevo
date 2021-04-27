331c557541d43fb01364a9584c0be22e6a3dc017
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-04-27 17:28:07 -0600

    Update Semana 6.R
    Conexión BD - SQL Server

--- Semana 6.R
+++ Semana 6.R
@@ -1,4 +1,7 @@
-plot(iris$Species)
-hist(iris$Sepal.Length)
-Hola 
-hola
+con <- DBI::dbConnect(odbc::odbc(),
+                      Driver   = "SQL Server",
+                      Server   = "dbtedamssql.uh.ac.cr",
+                      Database = "RstudioDB",
+                      UID      = rstudioapi::askForPassword("Database user"),
+                      PWD      = rstudioapi::askForPassword("Database password"),
+                      Port     = 1433)
