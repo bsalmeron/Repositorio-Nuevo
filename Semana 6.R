a188f307b95cc05caf86643590d1bcf3485b0f8d
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-04-27 18:16:03 -0600

    Update Semana 6.R

--- Semana 6.R
+++ Semana 6.R
@@ -1,7 +1,24 @@
 con <- DBI::dbConnect(odbc::odbc(),
                       Driver   = "SQL Server",
                       Server   = "dbtedamssql.uh.ac.cr",
-                      Database = "RstudioDB",
+                      Database = "Northwind",
                       UID      = rstudioapi::askForPassword("Database user"),
                       PWD      = rstudioapi::askForPassword("Database password"),
                       Port     = 1433)
+
+
+
+#Paquete
+install.packages("DBI")
+library("DBI")
+#Driver
+install.packages("odbc")
+library("odbc")
+
+ 
+
+comercio =  data.frame(tbl(con, "Orders Qry"))
+
+plot(as.factor(comercio$Country))
+
+?tbl
