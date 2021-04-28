93c012a8608032567e5830100b23240cf704120f
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-04-27 18:16:59 -0600

    Update Semana 6.R

--- Semana 6.R
+++ Semana 6.R
@@ -22,3 +22,18 @@ comercio =  data.frame(tbl(con, "Orders Qry"))
 plot(as.factor(comercio$Country))
 
 ?tbl
+
+
+#Práctica 
+
+1- Realice un analisis donde utilice lo aprendido hasta ahorita en especial
+las funciones de dplyr
+
+2. Genere un análsis exploratorio 
+
+3. Investigue como aplicaría un modelo de regresión lineal a su análisis 
+
+4. Use conexiones a base de datos 
+
+5. Use repostorios de datos para almacenar el proyecto realizado 
+
