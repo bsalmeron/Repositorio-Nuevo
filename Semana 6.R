cfd6e4cda88e94d8762265d7113bea9d3add2ea5
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-04-27 22:23:57 -0600

    Update Semana 6.R

--- Semana 6.R
+++ Semana 6.R
@@ -1,11 +1,3 @@
-con <- DBI::dbConnect(odbc::odbc(),
-                      Driver   = "SQL Server",
-                      Server   = "dbtedamssql.uh.ac.cr",
-                      Database = "Northwind",
-                      UID      = rstudioapi::askForPassword("Database user"),
-                      PWD      = rstudioapi::askForPassword("Database password"),
-                      Port     = 1433)
-
 
 
 #Paquete
@@ -15,15 +7,15 @@ library("DBI")
 install.packages("odbc")
 library("odbc")
 
- 
-
-comercio =  data.frame(tbl(con, "Orders Qry"))
-
-plot(as.factor(comercio$Country))
-
-?tbl
 
 
+con <- DBI::dbConnect(odbc::odbc(),
+                      Driver   = "SQL Server",
+                      Server   = "dbtedamssql.uh.ac.cr",
+                      Database = "AdventureWorksLT2017",
+                      UID      = rstudioapi::askForPassword("Pentaho_User"),
+                      PWD      = rstudioapi::askForPassword("Pentaho_User2021"),
+                      Port     = 1433)
 #Práctica 
 
 1- Realice un analisis donde utilice lo aprendido hasta ahorita en especial
@@ -36,4 +28,22 @@ las funciones de dplyr
 4. Use conexiones a base de datos 
 
 5. Use repostorios de datos para almacenar el proyecto realizado 
+
+#Intengración débil con fuentes externas de datos 
+
+consulta1= data.frame(tbl(con,"Usuario"))
+
+
+#Integración mas fuerte con SQL SERVER 
+
+#odbc, DBI, RSQLite y sqldf
+install.packages("")
+consulta2<-dbGetQuery(con ,"SELECT getdate()")
+View(consulta2)
+
+
+
+
+
+
 
